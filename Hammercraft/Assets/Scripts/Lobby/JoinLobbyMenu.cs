9555532e8d447e62caad6c17ac39cb4b13954315
Coppe Vincent <34739448+VincentCoppe@users.noreply.github.com>
2020-05-20 17:31:36 +0200

    #43 #41 connection hosting

---
+++ Hammercraft/Assets/Scripts/Lobby/JoinLobbyMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JoinLobbyMenu : MonoBehaviour
+{
+    [SerializeField] private NetworkManagerLobby NetworkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject LandingPagePanel = null;
+    [SerializeField] private TMP_InputField ipAddressInputField = null;
+    [SerializeField] private Button joinButton = null;
+
+    private void OnEnable()
+    {
+        NetworkManagerLobby.OnClientConnected += HandleClientConnected;
+        NetworkManagerLobby.OnClientDisconnected += HandleClientDisconnected;
+    }
+
+    private void OnDisable()
+    {
+        NetworkManagerLobby.OnClientConnected -= HandleClientConnected;
+        NetworkManagerLobby.OnClientDisconnected -= HandleClientDisconnected;
+    }
+
+    public void JoinLobby()
+    {
+        string ipAddress = ipAddressInputField.text;
+
+        NetworkManager.networkAddress = ipAddress;
+        NetworkManager.StartClient();
+
+        joinButton.interactable = false;
+    }
+
+    private void HandleClientConnected()
+    {
+        joinButton.interactable = true;
+
+        gameObject.SetActive(false);
+        LandingPagePanel.SetActive(false);
+    }
+
+    private void HandleClientDisconnected()
+    {
+        joinButton.interactable = true;
+    }
+}
---
+++ Hammercraft/Assets/Scripts/Lobby/MainMenuLobby.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuLobby : MonoBehaviour
+{
+    [SerializeField] private NetworkManagerLobby networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+
+    public void hostLobby()
+    {
+        networkManager.StartHost();
+
+        landingPagePanel.SetActive(false);
+    }
+}
