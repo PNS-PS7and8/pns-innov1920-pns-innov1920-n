00bc4e3dfe514052b0ea8ca2cecb7e90e50838a7
Florent ROBERT <florent.robert@etu.unice.fr>
2020-05-25 10:55:37 +0200

    #66 Unity test & Code coverage

---
+++ Hammercraft/Assets/Editor/BoardTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class BoardTest
+{
+    [Test]
+    public void BoardCreation_Test() {
+        Board board = new Board();
+        Assert.That(board.size, Is.EqualTo(new Vector2Int(50,50)));
+    }
+
+    [Test]
+        public void BoardCreationParam_Test() {
+        Board board = new Board(new Vector2Int(20,20));
+        Assert.That(board.size, Is.EqualTo(new Vector2Int(20,20)));
+    }
+}
