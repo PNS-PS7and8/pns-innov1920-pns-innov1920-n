82bc2b26e06ff07c120c246dddd6b61cef54b475
Florent ROBERT <florent.robert@etu.unice.fr>
2020-05-25 14:16:42 +0200

    #66 Class Player tests & Move tests to folder Test

---
+++ Hammercraft/Assets/Editor/Test/PlayerTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class PlayerTest : MonoBehaviour
+{
+    [Test]
+    public void Player1_Test(){
+        UnitCard u = ScriptableObject.CreateInstance<UnitCard>();
+        SpellCard s = ScriptableObject.CreateInstance<SpellCard>();
+        UnitCard[] us = new UnitCard[1];
+        SpellCard[] ss = new SpellCard[1];
+        us[0] = u;
+        ss[0] = s;
+        Deck deck = new Deck(us, ss);
+        Player p = new Player(deck);
+        Assert.That(p.originalDeck.DrawUnit(), Is.EqualTo(p.deck.DrawUnit()));
+    }
+
+        [Test]
+    public void Player2_Test(){
+        UnitCard u = ScriptableObject.CreateInstance<UnitCard>();
+        SpellCard s = ScriptableObject.CreateInstance<SpellCard>();
+        UnitCard[] us = new UnitCard[1];
+        SpellCard[] ss = new SpellCard[1];
+        us[0] = u;
+        ss[0] = s;
+        Deck deck = new Deck(us, ss);
+        Player p = new Player(deck);
+        Assert.That(p.originalDeck.DrawSpell(), Is.EqualTo(p.deck.DrawSpell()));
+    }
+}
