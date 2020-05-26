5a1ce09536c45976e4bcedff74a8e33e4c9db75c
Florent ROBERT <florent.robert@etu.unice.fr>
2020-05-26 12:10:50 +0200

    #66 PlayMode tests (unfinished)

---
+++ Hammercraft/Assets/Editor/Test/PlayModeTest/GameCardTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+[TestFixture]
+public class GameCardTests : MonoBehaviourBaseTest
+{
+    [UnityTest]
+    public bool GameCardUpdate_Test(){
+        Board board = new Board();
+        Cell cell = board.GetCell(1,1);
+        var gc = subject.AddComponent<GameCard>();
+        return true;
+    }
+
+}
---
+++ Hammercraft/Assets/Editor/Test/PlayModeTest/MonoBehaviourBaseTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Linq;
+using NUnit.Framework;
+
+public abstract class MonoBehaviourBaseTest
+{
+    protected GameObject subject;
+
+    [SetUp]
+    public virtual void SetUp(){
+        subject = new GameObject("Subject");
+    }
+
+    [TearDown]
+    public virtual void TearDown(){
+        Object.FindObjectsOfType<GameObject>().ToList().ForEach(go => Object.Destroy(go));
+    }
+
+}
