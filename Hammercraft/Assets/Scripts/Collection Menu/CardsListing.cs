ced2a369c06813d62c645edf6f4454c2ccc8b1a9
Barthélemy Passin-Cauneau <34715570+BarthelemyPassinCauneau@users.noreply.github.com>
2020-06-01 18:35:36 +0200

    #48 : display liste des cartes disponibles

---
+++ Hammercraft/Assets/Scripts/Collection Menu/CardsListing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardsListing : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _name;
+    [SerializeField] private TMP_Text _cost;
+    private Image image;
+
+    public void SetCardInfo(CardBase card) {
+        image = GetComponent<Image>();
+        image.color = (card is SpellCard)? Color.red : Color.blue;
+        _name.text = card.Name;
+        _cost.text = card.Cost.ToString();
+    }
+
+    public void on_click_card() {
+
+    }
+}
---
+++ Hammercraft/Assets/Scripts/Collection Menu/CardsListingMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CardsListingMenu : MonoBehaviour
+{
+    [SerializeField]
+    private CardsListing _cardsListing;
+    [SerializeField]
+    private Transform _content;
+
+    private Dictionary<string,CardsListing> ListCards = new Dictionary<string, CardsListing>();
+    void Start()
+    {
+        foreach (UnitCard card in Resources.LoadAll("Cards/Unit", typeof(UnitCard)))
+        {
+            ListCards[card.Name] = Instantiate(_cardsListing, _content);
+            ListCards[card.Name].SetCardInfo(card);
+        }
+
+        foreach (SpellCard card in Resources.LoadAll("Cards/Spell", typeof(SpellCard)))
+        {
+            ListCards[card.Name] = Instantiate(_cardsListing, _content);
+            ListCards[card.Name].SetCardInfo(card);
+        }        
+    }
+}
