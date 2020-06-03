fd3099fa1570f044e18369d34d5861ce6182b93d
Theo-Fafet <tfafet@gmail.com>
2020-06-03 10:56:48 +0200

    #91 add info gold

--- Hammercraft/Assets/Scripts/GameCore/InfoGold.cs
+++ Hammercraft/Assets/Scripts/GameCore/InfoGold.cs
@@ -3,8 +3,9 @@ using TMPro;
 
 public class InfoGold : BoardBehaviour
 {
+    [SerializeField] private PlayerRole role;
     [SerializeField] public TMP_Text text;
     private void Update() {
-        text.text = manager.CurrentPlayer.CurrentGold.ToString() + " / " + manager.CurrentPlayer.Gold.ToString();
+        text.text = manager.GetPlayer(role).CurrentGold.ToString() + " / " + manager.GetPlayer(role).Gold.ToString();
     }
 }
