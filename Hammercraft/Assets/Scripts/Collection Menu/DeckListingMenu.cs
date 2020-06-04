f53b46de5c85457b6c29c375384986faede1ce44
Florent ROBERT <florent.robert@etu.unice.fr>
2020-06-04 18:07:41 +0200

    #96 fix scene & mock first deck

--- Hammercraft/Assets/Scripts/Collection Menu/DeckListingMenu.cs
+++ Hammercraft/Assets/Scripts/Collection Menu/DeckListingMenu.cs
@@ -13,6 +13,21 @@ public class DeckListingMenu : MonoBehaviour
     public Dictionary<string,DeckListing> ListDecks = new Dictionary<string, DeckListing>();
     public Deck selectedDeck;
 
+    public void Start() {
+        UnitCard c1 = Resources.Load<UnitCard>("Cards/Unit/Noob");
+        UnitCard c2 = Resources.Load<UnitCard>("Cards/Unit/Fish");
+        UnitCard c3 = Resources.Load<UnitCard>("Cards/Unit/Eagle");
+        SpellCard c4 = Resources.Load<SpellCard>("Cards/Spell/Fireball");
+        SpellCard c5 = Resources.Load<SpellCard>("Cards/Spell/Heal");
+        SpellCard c6 = Resources.Load<SpellCard>("Cards/Spell/Rage");
+        UnitCard[] u1 = new UnitCard[] { c1, c1, c2, c2, c3 };
+        SpellCard[] s1 = new SpellCard[] { c4, c4, c5, c5, c6 };
+        Deck basicDeck1 = new Deck("Deck One", u1, s1);
+        DeckListing listing1 = Instantiate(_deckListing, _content);
+        ListDecks["Deck One"] = listing1;
+        ListDecks["Deck One"].SetDeckInfo(basicDeck1);
+    }
+
     private void OnEnable() {
         FetchServer();
     }
