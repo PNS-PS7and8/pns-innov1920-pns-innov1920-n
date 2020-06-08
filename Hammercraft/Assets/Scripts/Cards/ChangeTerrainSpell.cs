b2918649cd5f42658781048c7867c75b165cd80e
Florent ROBERT <florent.robert@etu.unice.fr>
2020-06-08 12:12:58 +0200

    #122 terraform aoe

--- Hammercraft/Assets/Scripts/Cards/ChangeTerrainSpell.cs
+++ Hammercraft/Assets/Scripts/Cards/ChangeTerrainSpell.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 [CreateAssetMenu(menuName="Cards/Spells/Terrain")]
-
 public class ChangeTerrainSpell : SpellCard
 {
     [SerializeField] private Cell.CellType terrain;
+    [SerializeField] private int range;
 
     protected override void CardEffect(Board board, Cell target, PlayerRole player)
     {
-        target.cellType = terrain;
+        List<Cell> cells = board.Disc(target, range).ToList();
+        foreach(Cell cell in cells){
+            cell.cellType = terrain;
+        }
     }
 }
